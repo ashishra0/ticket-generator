904ead53fb4e35f774e5a9499a643c39c98f9488
anik3tra0 <anik3tra0@gmail.com>
2017-05-18 18:46:45 +0530

    Refactored - Redo logic was not working, now has been fixed.

--- ticket_counter.rb
+++ ticket_counter.rb
@@ -1,11 +1,12 @@
 require './ticket'
+require 'pry'
 
 class TicketCounter
   def initialize(number_of_tickets)
     @tickets = []
     number_of_tickets.times do
       ticket = Ticket.new(nil, nil)
-      redo if (confirmed_tickets.map { |t| t.number }).include? ticket.number
+      redo if confirmed_tickets.map(&:number).include? ticket.number
       @tickets.push ticket
     end
     @tickets
@@ -24,8 +25,8 @@ class TicketCounter
   end
 
   def total
-    total = ('A'..'Z').count ** 3 * (('A'..'Z').count + (0..9).count) ** 3
-    last_three_count = (('A'..'Z').count + (0..9).count) ** 3
+    total = ('A'..'Z').count**3 * (('A'..'Z').count + (0..9).count)**3
+    last_three_count = (('A'..'Z').count + (0..9).count)**3
     invalid_count = Ticket::INVALID.count
     total - (last_three_count + invalid_count)
   end
@@ -39,13 +40,12 @@ class TicketCounter
   end
 
   def create_ticket
-    ticket = Ticket.new('CONFIRMED', Date.today.to_s) do
-      redo if (confirmed_tickets.map { |t| t.number }).include? ticket.number
-      redo if (cancelled_tickets.map { |t| t.number if t.date > (Date.today - 90).to_s }).include? ticket.number
-      redo if (all.map { |t| t.number if t.date > (Date.today - 730).to_s }).include? ticket.number
-    end
+    ticket = Ticket.new('CONFIRMED', Date.today.to_s)
+    create_ticket if confirmed_tickets.map(&:number).include? ticket.number
+    create_ticket if (cancelled_tickets.map { |t| t.number if t.date > (Date.today - 90).to_s }).include? ticket.number
+    create_ticket if (all.map { |t| t.number if t.date > (Date.today - 730).to_s }).include? ticket.number
     @tickets.push ticket
-    return ticket
+    ticket
   end
 
 end
